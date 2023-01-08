821a18017222511af576d79f8e9ce6f8dd61a1ed
Seunghyeon Yang <ysh410@gmail.com>
2023-01-09 02:14:53 +0900

    feat: change profile image

--- morethan-log.config.js
+++ morethan-log.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: '1yangsh',
-    image: '/avatar-me.svg',  // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: '/avatar.svg',  // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: 'Data Engineer',
     bio: 'Data Engineering',
     email: '1yangsh@gmail.com',
