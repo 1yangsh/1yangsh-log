93abd6a1f323069e7c3e063316a6a05d90bf6536
Seunghyeon Yang <ysh410@gmail.com>
2023-01-08 23:50:33 +0900

    feat: change config

--- morethan-log.config.js
+++ morethan-log.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: 'morethanmin',
+    name: '1yangsh',
     image: '/avatar.svg',  // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: 'frontend developer',
-    bio: 'I develop everything using node.',
-    email: 'morethanmin.dev@gmail.com',
-    linkedin: 'morethanmin',
-    github: 'morethanmin',
+    role: 'Data Engineer',
+    bio: 'Data Platform Engineering',
+    email: '1yangsh@gmail.com',
+    linkedin: '1yangsh',
+    github: '1yangsh',
     instagram: '',
   },
   projects: [
@@ -18,13 +18,13 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: 'morethan-log',
-    description: 'welcome to morethan-log!',
+    title: '1yangsh-log',
+    description: 'welcome to 1yangsh-log!',
     theme: 'light' // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
-  link: 'https://morethan-log.vercel.app',
+  link: 'https://1yangsh-blog.vercel.app',
   since: 2022, // If leave this empty, current year will be used.
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: 'https://og-image-korean.vercel.app', // The link to generate OG image, don't end with a slash
@@ -53,7 +53,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: 'morethanmin/morethan-log',
+      repo: '1yangsh/1yangsh-log',
       'issue-term': 'og:title',
       label: '💬 Utterances',
     }
